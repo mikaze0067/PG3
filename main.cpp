d5056d63f2be570a5c6342c7d7d669a5672669c9
mikaze <146914469+mikaze0067@users.noreply.github.com>
2024-10-23 11:26:05 +0900

    01-04完了 多分

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<functional>
+#include <stdio.h>
+#include <functional>
 #include <windows.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,48 +8,56 @@ typedef void(*PFunc)(int*);
 
 // コールバック関数
 void DispResult(int* result) {
-	if (*result % 2 == 0) {
-		printf("サイコロの出目は %d です。偶数！\n", *result);
-	}
-	else {
-		printf("サイコロの出目は %d です。奇数！\n", *result);
-	}
+    if (*result % 2 == 0) {
+        printf("サイコロの出目は %d です。偶数！\n", *result);
+    }
+    else {
+        printf("サイコロの出目は %d です。奇数！\n", *result);
+    }
 }
 
 void setTimeout(PFunc p, int second) {
-	//コールバック関数を呼び出す
-	Sleep(second * 1000);
-	p(&second);
+    //コールバック関数を呼び出す
+    Sleep(second * 1000);
+    p(&second);
 }
 
 int main(int argc,const char *argv[]) {
+   
 
-	std::function<int(int)> fx = [](int i) {return i + 1; };
-	auto fx2 = [](int i) {return i + 1; };
+    int dice;
+    int num;
 
+    // サイコロを振る
+    srand((unsigned int)time(NULL));
+    dice = rand() % 6 + 1;  // 1から6のランダムな数字を生成
 
-	int dice;
-	int n;
+    printf("サイコロの出目が半(奇数)か丁(偶数)かを当ててください\n");
+    printf("1が半(奇数), 2が丁(偶数): ");
+    scanf_s("%d", &num);
+   
+    // ユーザー入力のチェックを行うラムダ関数（1か2のみを許容）
+    std::function<int(int)> validateInput = [](int n) {
+        if (n != 1 && n != 2) {
+             // ユーザーに1か2（奇数か偶数か）で入力させる
+            printf("無効な入力です。1（奇数）か2（偶数）を入力してください。\n");
+        }
+        return n;
+        };
 
-	// サイコロを振る
-	srand((unsigned int)time(NULL));
-	dice = rand() % 6 + 1;
+    // ユーザーの入力を検証
+    num = validateInput(num);
 
-	// ユーザーに1か2で入力させる
-	printf("サイコロの出目が半(奇数)か丁(偶数)かを当ててください\n");
-	printf("1が半(奇数), 2が丁(偶数): ");
-	scanf_s("%d", &n);
+    // 3秒後に結果を表示
+    setTimeout(DispResult, 3);
 
-	// 3秒後に結果を表示
-	setTimeout(DispResult, 3);
+    // ユーザーの回答を判定
+    if ((dice % 2 == 1 && num == 1) || (dice % 2 == 0 && num == 2)) {
+        printf("正解です！\n");
+    }
+    else {
+        printf("不正解です！\n");
+    }
 
-	// ユーザーの回答を判定
-	if ((dice == 1 && n == 2) || (dice == 2 && n == 1) || (dice == 3 && n == 2) || (dice == 4 && n == 1) || (dice == 5 && n == 2) || (dice == 6 && n == 1)) {
-		printf("正解\n");
-	}
-	else {
-		printf("不正解\n");
-	}
-
-	return 0;
+    return 0;
 }
